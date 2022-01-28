b3b2e36a92e170d6dcd297f5d1b6de2641d11d2e
Akshay <akshaykhandelwal79@gmail.com>
2022-01-28 20:22:40 +0530

    initial commit

---
+++ provider.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
+  region                  = "ap-south-1"
+  shared_credentials_file = "~/.aws/creds"
+  profile                 = "vscode"
+}
